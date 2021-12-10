aa6828a2c9337290d15e289fb01595b9a4f32d74
Akos Gabriel <akos.gabriel@dpc.hu>
2021-12-10 12:14:55 +0000

    scripts added

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source env.sh
+export FLASK_ENV=development
 flask run --host 0.0.0.0
 
---
+++ run_cli.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export AWS_DEFAULT_REGION="eu-central-1"
+python3 cli.py -s s9oova2lmhfvuj1p0lo977oh0ac8vb00on93gofnrf6lrdlvrc9 -c 47d0it9bhpvkcg218dofq67leo -u akos
+
