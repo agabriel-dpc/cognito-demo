a0b1c5d7a7173ed3b66115352a7966ba10242952
Akos Gabriel <akos.gabriel@dpc.hu>
2022-01-14 10:16:46 +0000

    dev

---
+++ aws/env2.sh
@@ -0,0 +1,6 @@
+export COGNITO_DEMO_USERNAME="akos.gabriel@dpc.hu"
+export COGNITO_DEMO_S3_BUCKET_NAME="dev-rina-mfa2"
+export COGNITO_DEMO_LOGIN_DOMAIN_PREFIX="dev-rina-mfa2"
+export AWS_DEFAULT_REGION="eu-central-1"
+export COGNITO_DEMO_SETTINGS="settings.cfg"
+
