a4a66c5c84ddbc0fb64cfc19bd6eaaa7d9df5e31
Akos Gabriel <akos.gabriel@dpc.hu>
2022-01-15 00:15:02 +0000

    Add config for second pool (staging)

---
+++ env2.sh
@@ -0,0 +1,6 @@
+export COGNITO_DEMO_USERNAME="akos.gabriel@dpc.hu"
+export COGNITO_DEMO_S3_BUCKET_NAME="dev-rina-mfa"
+export COGNITO_DEMO_LOGIN_DOMAIN_PREFIX="rina-nonprod-mfa-auth"
+export AWS_DEFAULT_REGION="eu-central-1"
+export COGNITO_DEMO_SETTINGS="settings.local2.cfg"
+
